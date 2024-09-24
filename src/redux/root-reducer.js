16bf361b30203298b2182d4bfea2780cb605e7bb
Saim Shahzad <saimshehzad2030@gmail.com>
2024-09-24 11:33:39 +0500

    few changes

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "./reducers/cart-reducer";
+import orderReducer from "./reducers/order-reducer";
 import userReducer from "./reducers/user.reducer";
 export default combineReducers({
   userCart: cartReducer,
   user: userReducer,
+  userOrders: orderReducer,
 });
