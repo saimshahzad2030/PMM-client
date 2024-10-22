87c931f018d71becf4a67383697fcd639443857c
Saim Shahzad <saimshehzad2030@gmail.com>
2024-10-22 15:28:28 +0500

    few changes

--- src/app/api/page.js
+++
@@ -1,7 +0,0 @@
-// import store from "../../redux/store";
-
-// export default function handler(req, res) {
-//   const state = store.getState();
-//   const mode = state.mode; // Access your mode state here
-//   res.status(200).json({ mode });
-// }
